9410d2cffd37c4d370f6dcf3891174fb384b628e
ndvo <findme@kevinvo.xyz>
2020-04-01 03:09:29 -0700

    Update Express

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
-// const logging = require('./src/common/loggingUtils');
-import logging from "./src/common/loggingUtils";
-import createError from "http-errors";
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
 
+import * as logging from './src/common/loggingUtils';
+import express from 'express';
 const logger = logging.getLogger('MainApp');
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-var app = express();
-
-
-
-app.use(logging.expressLogger);
+// body parser json
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
+// Setup log for app
+app.use(logging.expressLogger);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get('/helloworld', (req, res) => {
+    res.send('Hello World');
 });
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
-
-
-module.exports = app;
