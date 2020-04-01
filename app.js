907ae87d0de7428a5c05b9d350ce0ada80884bab
ndvo <findme@kevinvo.xyz>
2020-03-31 21:14:02 -0700

    Setup Express

---
+++ app.js
@@ -0,0 +1,46 @@
+// const logging = require('./src/common/loggingUtils');
+import logging from "./src/common/loggingUtils";
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+
+const logger = logging.getLogger('MainApp');
+
+
+var app = express();
+
+
+
+app.use(logging.expressLogger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+
+
+module.exports = app;
---
+++ src/common/loggingUtils.js
@@ -0,0 +1,25 @@
+import log4js from 'log4js';
+
+let defaultLevel = 'debug';
+
+console.log('http://localhost:3000');
+console.log(`log default level is :  ${defaultLevel}`);
+
+log4js.configure({
+    levels: { info: { value: 20000, colour: 'green' } },
+    appenders: { err: { type: 'stderr' } },
+    categories: { default: { appenders: ['err'], level: defaultLevel } }
+});
+
+const getLogger = log4js.getLogger;
+const connectLogger = log4js.connectLogger;
+
+export default {
+    getLogger,
+    connectLogger,
+    expressLogger: log4js.connectLogger(log4js.getLogger('express'), {
+        level: log4js.levels.INFO,
+        format: (req, res, format) => format(`":method :url"\n_query: ${JSON.stringify(req.query)}\n_body(post method only): ${JSON.stringify(req.body)}`)
+    })
+};
+
