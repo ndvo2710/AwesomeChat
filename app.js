30953ba02cc5638dd996ee796132103a237395ae
ndvo <findme@kevinvo.xyz>
2020-04-05 04:19:08 -0700

    [1.0.0] Add static pages

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 import * as logging from './src/common/loggingUtils';
 import express from 'express';
 import connectDB from './src/config/connectDB';
-import ContactModel from './src/models/contact.model';
+import configViewEngine from './src/config/viewEngine';
 
 const logger = logging.getLogger('MainApp');
 
 const app = express();
+const port = process.env.PORT || 3000;
+// body parser json
+app.use(express.json());
+// Setup log for app
+app.use(logging.expressLogger);
 
 // Connect to MongoDB
 connectDB();
 
-const port = process.env.PORT || 3000;
+// Config view engine
+configViewEngine(app);
 
-// body parser json
-app.use(express.json());
-
-// Setup log for app
-app.use(logging.expressLogger);
-
-app.get('/test-database', async (req, res) => {
-    try {
-        const item = {
-            userId: '418443231',
-            contactId: '59289484343524859425'
-        };
-        const contact = await ContactModel.createNew(item);
-        logger.info(`New contact is \n ${contact}`);
-        res.send(contact);
-    } catch (e) {
-        logger.debug(e);
-    }
+app.get('/', (req, res) => {
+    return res.render("main/master");
 });
+
+app.get('/login-register', (req, res) => {
+    return res.render("auth/loginRegister");
+});
+
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
