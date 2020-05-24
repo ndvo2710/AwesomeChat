a3e73829db9c4ab4d77b9206e665cd441cca4a17
Kevin Nguyen Vo <nguyen.vo@oculus.com>
2020-05-24 06:29:11 -0700

    [16] Update avatar and info of user front end

--- src/app.js
+++ src/app.js
@@ -11,77 +11,77 @@ import initRoutes from './routes/web';
 import configSession from './config/session';
 
 const logger = logging.getLogger('MainApp');
-
-pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
-  if (err) {
-    throw err;
-  }
-
-  // Init App
-  const app = express();
-  const port = process.env.PORT || 3000;
-
-  // Setup log for app
-  app.use(logging.expressLogger);
-
-  // Connect to MongoDB
-  connectDB();
-
-  // Config Session
-  configSession(app);
-
-  // Config view engine
-  configViewEngine(app);
-
-  // Enable post data for request
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  // Enable flash messages
-  app.use(connectFlash());
-
-  // Config passport js
-  app.use(passport.initialize());
-  app.use(passport.session());
-
-  // Init all routes
-  initRoutes(app);
-
-  https
-    .createServer({ key: keys.serviceKey, cert: keys.certificate }, app)
-    .listen(port, () => {
-      logger.info(`Server is up on port ${port}`);
-    });
-});
-
-// // Init App
-// const app = express();
-// const port = process.env.PORT || 3000;
 //
-// // Setup log for app
-// app.use(logging.expressLogger);
+// pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+//   if (err) {
+//     throw err;
+//   }
 //
-// // Connect to MongoDB
-// connectDB();
+//   // Init App
+//   const app = express();
+//   const port = process.env.PORT || 3000;
 //
-// // Config Session
-// configSession(app);
+//   // Setup log for app
+//   app.use(logging.expressLogger);
 //
-// // Config view engine
-// configViewEngine(app);
+//   // Connect to MongoDB
+//   connectDB();
 //
-// // Enable post data for request
-// app.use(bodyParser.urlencoded({ extended: true }));
+//   // Config Session
+//   configSession(app);
 //
-// // Enable flash messages
-// app.use(connectFlash());
+//   // Config view engine
+//   configViewEngine(app);
 //
-// // Config passport js
-// app.use(passport.initialize());
-// app.use(passport.session());
+//   // Enable post data for request
+//   app.use(bodyParser.urlencoded({ extended: true }));
 //
-// // Init all routes
-// initRoutes(app);
+//   // Enable flash messages
+//   app.use(connectFlash());
 //
-// app.listen(port, () => {
-//   logger.info(`Server is up on port ${port}`);
+//   // Config passport js
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+//
+//   // Init all routes
+//   initRoutes(app);
+//
+//   https
+//     .createServer({ key: keys.serviceKey, cert: keys.certificate }, app)
+//     .listen(port, () => {
+//       logger.info(`Server is up on port ${port}`);
+//     });
 // });
+
+// Init App
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Setup log for app
+app.use(logging.expressLogger);
+
+// Connect to MongoDB
+connectDB();
+
+// Config Session
+configSession(app);
+
+// Config view engine
+configViewEngine(app);
+
+// Enable post data for request
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable flash messages
+app.use(connectFlash());
+
+// Config passport js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Init all routes
+initRoutes(app);
+
+app.listen(port, () => {
+  logger.info(`Server is up on port ${port}`);
+});
